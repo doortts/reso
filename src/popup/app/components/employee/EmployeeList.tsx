a0d26bd5bd6841e0ec0c0c0fb7f62352454b4700
Suwon Chae <doortts@gmail.com>
2019-12-08 18:49:53 +0900

    polishing: Fix indentation

--- src/popup/app/components/employee/EmployeeList.tsx
+++ src/popup/app/components/employee/EmployeeList.tsx
@@ -9,22 +9,21 @@ export const EmployeeList = React.memo((): any => {
 
   return useObserver(() => (
     store.employees.map((employee: IEmployee) => {
-        let isSelected = false
+      let isSelected = false
 
-        // TODO: Move to store
-        if (store.selectedEmployeeIndex >= 0 && store.employees[store.selectedEmployeeIndex].mail === employee.mail) {
-          isSelected = true
-        }
-
-        return (
-          <EmployeeContainer
-            key={employee.uid}
-            employee={employee}
-            servers={store.githubServers}
-            isSelected={isSelected}
-          />
-        )
+      // TODO: Move to store
+      if (store.selectedEmployeeIndex >= 0 && store.employees[store.selectedEmployeeIndex].mail === employee.mail) {
+        isSelected = true
       }
-    )
+
+      return (
+        <EmployeeContainer
+          key={employee.uid}
+          employee={employee}
+          servers={store.githubServers}
+          isSelected={isSelected}
+        />
+      )
+    })
   ))
 })
