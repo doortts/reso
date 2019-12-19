52024b9234f845ab70ad00d9989ca4b24101b4ad
Suwon Chae <doortts@gmail.com>
2019-12-19 10:14:27 +0900

    ui: Introduce dynamic selection bar color

--- src/popup/app/components/employee/Employee.tsx
+++ src/popup/app/components/employee/Employee.tsx
@@ -13,10 +13,34 @@ interface Props {
   handleClick: (e: React.MouseEvent<HTMLElement>) => void
 }
 
+const getRandomBg = () => {
+  const bgColors = [
+    'linear-gradient(90deg, rgba(217,255,16,1) 0%, rgba(198,40,40,1) 100%)',
+    'linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)',
+    'radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)',
+    'radial-gradient(circle, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)',
+    'linear-gradient(90deg, rgba(5,191,231,1) 0%, rgba(29,79,253,1) 50%, rgba(252,69,85,1) 100%)'
+  ]
+  const randomIndex = Math.floor(Math.random() * Math.floor(bgColors.length))
+  return bgColors[randomIndex]
+}
+
+const selectedStyle = (): any => ({
+  background: getRandomBg(),
+  backgroundBlendMode: 'multiply',
+  backgroundPosition: 'center center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  boxShadow: '#444444 1px 1px 2px 0px',
+  boxSizing: 'border-box'
+})
+
+const bgColor = selectedStyle()
+
 export const Employee: React.FC<Props> = observer<Props>(props => {
   const { employee, handleClick, isSelected } = props
 
-  let focusStyle = isSelected ? selectedStyle(employee.photoUrl) : {}
+  let focusStyle = isSelected ? bgColor : {}
 
   const store = useStore()
   const classes = useStyles()
@@ -61,17 +85,6 @@ export const Employee: React.FC<Props> = observer<Props>(props => {
       <EmployeeView employee={employee} />
     </ListItem>
   )
-})
-
-const selectedStyle = (photoUrl: string | undefined = ''): any => ({
-  backgroundColor: '#f1f1f1',
-  backgroundImage: `url("${photoUrl}")`,
-  backgroundBlendMode: 'multiply',
-  backgroundPosition: 'center center',
-  backgroundRepeat: 'no-repeat',
-  backgroundSize: 'cover',
-  boxShadow: '#444444 1px 1px 2px 0px',
-  boxSizing: 'border-box',
 })
 
 const useStyles = makeStyles((theme: Theme) => ({
