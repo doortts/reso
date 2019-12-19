f4cabc47cf684db4e05c4053442fe75830343a60
Suwon Chae <doortts@gmail.com>
2019-12-19 11:21:40 +0900

    ui: Redesign employee detail ui

--- src/popup/app/components/employee/EmployeeDetail.tsx
+++ src/popup/app/components/employee/EmployeeDetail.tsx
@@ -2,6 +2,16 @@ import React from 'react'
 import { useObserver } from 'mobx-react-lite'
 
 import { useStore } from '../../context'
+import {
+  Button,
+  Card, CardActions,
+  CardContent,
+  CardMedia,
+  createStyles,
+  makeStyles,
+  Theme,
+  Typography, useTheme
+} from '@material-ui/core'
 
 declare module 'react' {
   interface ImgHTMLAttributes<T> extends HTMLAttributes<T> {
@@ -13,38 +23,85 @@ const addDefaultSrc = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
   (e.target as HTMLImageElement).src = '/images/default-avatar-64.png'
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    card: {
+      display: 'flex',
+    },
+    details: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    content: {
+      flex: '1 0 auto',
+    },
+    cover: {
+      width: 200,
+    },
+    controls: {
+      display: 'flex',
+      alignItems: 'center',
+      paddingLeft: theme.spacing(1),
+      paddingBottom: theme.spacing(1),
+    },
+    playIcon: {
+      height: 38,
+      width: 38,
+    },
+  }),
+)
+
 export const EmployeeDetail = () => {
   const store = useStore()
 
-  const handleClick = () => {
-    store.inputRef?.current?.focus()
-  }
+  const classes = useStyles()
+  const theme = useTheme()
 
   return useObserver(() => {
       let employee = store.getSelectedEmployee()
       if (!employee) {
         return (<div></div>)
       }
-      console.log('selected employee', employee?.displayName)
-      const { company, employeeNumber, displayName, photoUrl, department, mail } = employee
+      const {
+        company,
+        employeeNumber,
+        displayName,
+        photoUrl,
+        department,
+        mail,
+        telephoneNumber
+      } = employee
 
       return (
         <div style={{ position: 'sticky', top: '70px' }}>
-          <div><h1>Detail</h1></div>
-          <div>
-            <img
-              src={photoUrl}
-              onError={addDefaultSrc}
-              loading="lazy"
-              alt="Employee photo"
+          <Card className={classes.card}>
+            <CardMedia
+              className={classes.cover}
+              image={photoUrl}
+              title="Employee Photo"
             />
-          </div>
-          <div>{company} </div>
-          <div>{displayName} </div>
-          <div>{employeeNumber}</div>
-          <div>{department} </div>
-          <div>{mail}</div>
-          <div><button onClick={handleClick} >click</button></div>
+            <div className={classes.details}>
+              <CardContent className={classes.content}>
+                <Typography component="h5" variant="h5">
+                  {displayName}
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                  <div>{company} </div>
+                  <div style={{ color: '#2196F3' }}>{employeeNumber}</div>
+                  <div>{department} </div>
+                  <div>{telephoneNumber} </div>
+                  <div>{mail}</div>
+                </Typography>
+              </CardContent>
+              <div className={classes.controls}>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Show More
+                  </Button>
+                </CardActions>
+              </div>
+            </div>
+          </Card>
         </div>
       )
     }
